0b856b7a0fa89a9b0db0018e53cdac3453a91569
Filip <filipkaczmarek@macbook-air.home>
2021-03-26 18:58:28 +0100

    Hamburger Button - Mobile | Done

--- src/navMobile.js
+++ src/navMobile.js
@@ -1,5 +1,6 @@
 const hamburgerButton = document.querySelector('.navbar-mobile__menu');
 const listofElements = document.querySelector('.navbar-lists');
+const listItem = [...document.querySelectorAll('.navbar-list__item')];
 
 hamburgerButton.addEventListener('click', function(){
 
@@ -10,5 +11,15 @@ hamburgerButton.addEventListener('click', function(){
         listofElements.classList.remove('navbar-lists--showed');
         listofElements.classList.add('navbar-lists--hidden');
     }
+}, false);
 
-}, false)
+for( let i = 0; i < listItem.length; i += 1 ){
+
+    listItem[i].addEventListener('click', function(){
+    
+        listofElements.classList.remove('navbar-lists--showed');
+        listofElements.classList.add('navbar-lists--hidden');
+    
+    }, false)
+
+}
